d6bd38c0681aeeca0cdeca40166bc4a76b66e1fc
Justin Jung <justinsoljung@gmail.com>
2024-07-21 14:26:14 -0700

    Add game engine and GPT model class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import json 
 import pdb 
+from model import Replanner
 
 '''
 I want to take a list of words, generate some initial plan, and then be able to evaluate the plan. 
@@ -101,51 +102,88 @@ def predict_groups(words, groups_correct, failed_words=[], history=None, self_re
         raise TypeError("GPT returned wrong json type")
     return response_json, history
 
-def execute_pred_groups(pred_groups):
-    '''
-    Given a list of predicted group of words, execute in the browser
 
-    pred_groups: List[Dict[str: List[str]]] each object is {category_theme: [words]}
-    '''
-    def get_result(group_words):
-        print(group_words)
-        user_input = input("Was it failed, Y or N: ")
-        return True if user_input == "Y" or user_input == "y" else False 
+
+class Engine:
+    def __init__(self, all_words: list[str]):
+        self.groups_correct = 0
+        self.num_mistakes = 0
+        self.remaining_words = all_words
+        self.planner = Replanner(self.remaining_words)
     
-    num_solved = 0
-    solved_words = set()
-    for i in range(0, len(pred_groups)):
-        category, group_words = list(pred_groups[i].items())[0]
-        failed = get_result(group_words)
-        if failed:
-            return num_solved, group_words, solved_words
+    def execute_plan(self, plan):
+        # Returns a list of idxs of success groups
+        # plan: List[Dict[str: List[str]]] each object is {category_theme: [words]}
+
+        def get_result(group_words):
+            print(group_words)
+            user_input = input("Was it failed, Y or N: ")
+            return True if user_input == "Y" or user_input == "y" else False 
+        
+        successful_idxs = []
+        for i in range(0, len(plan)):
+            category, group_words = list(plan[i].items())[0]
+            failed = get_result(group_words) #TODO: add multion integration
+            if failed:
+                break 
+            
+            successful_idxs.append(i)
+        return successful_idxs
+        
+
+
+    def get_remaining_words(self, plan, success_group_idxs):
+        solved_words = set()
+        for idx in success_group_idxs:
+            category, group = list(plan[idx].items())[0]
+            solved_words.update(set(group))
+        
+        return list(set(self.remaining_words) - solved_words)
+
+
+    def get_failed_group(self, plan, success_group_idxs):
+        # Returns the failed {category: [group_words]} group 
+        if len(success_group_idxs) == 0:
+            failed_idx = 0
         else:
-            num_solved += 1  
-            solved_words.update(set(group_words))
+            failed_idx = max(success_group_idxs) + 1
+            assert self.groups_correct < 4
+        
+        return plan[failed_idx]
+    
+    def update_state(self, plan, success_group_idxs):
+        self.groups_correct += len(success_group_idxs)
+        # check if game is over
+        if self.groups_correct == 4: return  
 
-    _, last_group = list(pred_groups[num_solved-1].items())[0]
-    return num_solved, last_group, solved_words
+        # update the remaining words and add failed group to planner memory
+        remaining_words = self.get_remaining_words(plan, success_group_idxs)
+        self.remaining_words = remaining_words
+        self.planner.update_all_words(remaining_words)
+        failed_group = self.get_failed_group(plan, success_group_idxs)
+        self.planner.update_failed_group(failed_group)
+        
+        # increment mistakes
+        self.num_mistakes += 1
+    
+    def main(self):
+        while(self.groups_correct < 4 and self.num_mistakes < 4):
+            generated_plan = self.planner.driver()
+            success_group_idxs = self.execute_plan(generated_plan)
+            self.update_state(generated_plan, success_group_idxs)
+        
+        if self.groups_correct == 4:
+            print("Congratulations! You solved the puzzle.")
+        else:
+            print("Try again next time.")
+        
 
-def main(all_words):
-    '''
-    all_words: List[str]
-    '''
-    groups_correct, num_mistakes = 0, 0
-    remaining_words = all_words[:]
-    failed_words = []
-    history = None 
-    while (groups_correct < 4 and num_mistakes < 4):
-        pred_groups, history = predict_groups(remaining_words, groups_correct, failed_words=failed_words, history=history)        
-        num_groups_solved, failed_words, visited_words = execute_pred_groups(pred_groups)
-        remaining_words = list(set(remaining_words) - visited_words)
-        groups_correct += num_groups_solved
-        num_mistakes += 1
-    if groups_correct == 4:
-        print("Congratulations! You solved the puzzle.")
-    else:
-        print("Try again next time.")
+
+    
 
 
 if __name__ == "__main__":
     words = ["WAX", "MUMMY", "GIFT", "ANCHOR", "BURRITO", "PRESENT", "CLAY", "PAPYRUS", "SPRAIN", "FLAIR", "MODERATE", "TALENT", "INSTINCT", "PARCHMENT", "HOST", "FACULTY"]
-    main(words)
+    game_engine = Engine(words)
+    pdb.set_trace()
+    game_engine.main()
--- model.py
+++ model.py
@@ -7,6 +7,8 @@ import json
 import pdb 
 from collections import defaultdict
 import math 
+from constants import incorrect_json_str, plan_generator_system_prompt, replan_generator_system_prompt
+
 load_dotenv()
 #TODO: use different model types 
 class Jury:
@@ -90,12 +92,105 @@ class Jury:
 
         return is_valid
 
+
+class GPT:
+    def __init__(self, user_prompt, system_prompt, failed_plans, model_type='gpt-4o'):
+        self.user_prompt = user_prompt
+        self.system_prompt = system_prompt
+        self.failed_plans = failed_plans
+        if self.failed_plans:
+            self.history = [
+                    {"role": "system", "content": system_prompt},
+                    {"role": "system", "content": f'Previous failed list of groups: """{failed_plans}"""'},
+                    {"role": "user", "content": user_prompt}
+            ]
+        else:
+            self.history = [
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt}
+            ]
+        self.client = OpenAI()
+        self.model_type = model_type
+    
+    def return_json(self):
+        response = self.client.chat.completions.create(
+            model=self.model_type,
+            messages=self.history,
+            response_format={ "type": "json_object" },
+        )
+        response_msg = response.choices[0].message.content
+        self.history.append({"role": "assistant", "content": response_msg})
+        response_json = json.loads(response_msg)
+        return response_json
+    
+    def check_disjoint_sets(self, list_of_sets):
+        for i in range(len(list_of_sets)):
+            for j in range(i + 1, len(list_of_sets)):
+                if list_of_sets[i] & list_of_sets[j]:  # Check for intersection
+                    return False
+        return True
+
+    
+    def check_valid_json(self, output, board_words):
+        # output: (dict) Has key: 'groups'
+        # Checks if the loaded json is valid. Returns boolean
+        if isinstance(output, dict) and 'groups' in list(output.keys()):
+            plan = output['groups']
+        else:
+            print("Output not in json format with key groups")
+            return False 
+        
+        list_groups = [] # list of all the group words as sets
+        # check if created right number of groups
+        if len(board_words) // 4 != len(plan):
+            print("Created wrong number of groups.")
+            return False 
+
+        # check if each group has four words 
+        for i in range(len(plan)):
+            category, group_words = list(plan[i].items())[0]
+            list_groups.append(set(group_words))
+            if len(group_words) != 4:
+                print("Not all groups have length 4")
+                return False 
+            
+            # check if group words come from board words
+            for word in group_words:
+                if word not in board_words: 
+                    print("Group word not from board words")
+                    return False 
+
+        
+        all_disjoint = self.check_disjoint_sets(list_groups)
+        if not all_disjoint:
+            print("Groups aren't disjoint")
+            return False  
+        return True  
+
+    def forward(self, board_words):
+        '''
+        Generates responses until you get a valid response. Returns plan which is a list of [{category: [group_words]]
+        '''
+        is_valid = False
+        while (not is_valid):
+            output = self.return_json()
+            is_valid = self.check_valid_json(output, board_words)
+
+            if not is_valid:
+                self.history.append({"role": "user", "content": incorrect_json_str})
+                print(f"GPT returned an invalid response.\n")
+                print()
+            
+        return output['groups'] 
+
+
 class Replanner:
     # generates one plan that is validated as correct
     def __init__(self, all_words: list[str]) -> None:
-        self.all_words = all_words
+        self.all_words = all_words #all words remaining on the board 
         self.jury_failed_groups = [] # list of voted failed {category: [group_words] } groups  
         self.failed_groups = [] # list of env failed {category: [group_words] }
+        self.failed_plans = [] #list of failed plans [{category: [group_words] }]
         self.jury = Jury()
     
     def update_jury_failed_groups(self, plan, is_valid_arr):
@@ -103,63 +198,39 @@ class Replanner:
             if not is_valid_arr[i]:
                 self.jury_failed_groups.append(plan[i])
         return 
+    
+    def update_failed_group(self, group_dict):
+        # adds {category: [group_words] } to the history 
+        self.failed_groups.append(group_dict)
+    
+    def update_all_words(self, remaining_words):
+        self.all_words = remaining_words
+
+    def update_failed_plans(self, plan):
+        self.failed_plans.append(plan)
 
 
 
     def generate_plan(self):
         '''
-        Generates a list (denoted plan) of four [{category: [group_words] }] using all words
+        Generates a list (denoted plan) of [{category: [group_words] }] using all words remaining on board
             Replans if 2,3 generated categories makes sense, returns plan if all make sense,
             returns None if <= 1 generated categories make sense
         '''
-        client = OpenAI()
-        system_prompt = f'''You are a smart NYT Connections puzzle solver. You are given a set of words and\
-            from those words you will generate groups of four words, where each group has some category theme and\
-                  the groups have no overlapping words.
+        user_prompt = f'Use these set of words to generate groups of four from: """{self.all_words}"""'
         
-        Return the groups as JSON object with a single key 'groups' whose value is an array of objects. Each object should have a single key-value pair, where the key is the category theme and the value is an array of 4 words in the group.
-        word_set = ['CAMPAIGN', 'CANVASS', 'CLAMP', 'COMPOSITION', 'FABRIC', 'FILE', 'LEVEL', 'LOG', 'MAKEUP', 'MAX', 'MOD', 'ORGANIZE', 'SAW', 'STRUCTURE', 'STUMP', 'TAN']
-        Return:
-        {{"groups": [
-            {{
-                "WAYS TO SUPPORT A CANDIDATE": ["CAMPAIGN", "CANVASS", "ORGANIZE", "STUMP"]
-            }},
-            {{
-                "CONSTITUTION": ["COMPOSITION", "FABRIC", "MAKEUP", "STRUCTURE"]
-            }},
-            {{
-                "CARPENTRY TOOLS": ["CLAMP", "FILE", "LEVEL", "SAW"]
-            }},
-            {{
-                "MATH ABBREVIATIONS": ["LOG", "MAX", "MOD", "TAN"]
-            }}
-        ]}}
-        Always return valid JSON in this format.
-        '''
-        
-        user_prompt = f"Set of words to generate groups of 4 from: {self.all_words}"
-        history = [
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt}
-            ]
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=history,
-            response_format={ "type": "json_object" },
-        )
-        response_msg = response.choices[0].message.content
-        response_json = json.loads(response_msg)
-        if isinstance(response_json, dict) and 'groups' in list(response_json.keys()):
-            plan = response_json['groups']
-        else:
-            raise ValueError(f"GPT generated the wrong JSON output. {response_msg}")
+        gpt_gen = GPT(user_prompt, plan_generator_system_prompt, self.failed_plans)
+        plan = gpt_gen.forward(self.all_words)
+        print(f"Generated Plan: {plan}\n")
         
         # evaluate the generated groups
         is_valid_arr = self.jury.get_verdict(plan)
         num_valid = sum(map(int, is_valid_arr))
+        print(f"Jury verdict: {is_valid_arr}")
 
         if num_valid <= 1:
             self.update_jury_failed_groups(plan, is_valid_arr)
+            self.update_failed_plans(plan)
             return None  
         elif num_valid < 4:
             used_words = set()
@@ -176,6 +247,7 @@ class Replanner:
             if regen_result is None:
                 # TODO: if regen fails, we might assume the first "correct" ones are false
                 self.update_jury_failed_groups(plan, is_valid_arr)
+                self.update_failed_plans(plan)
                 return None
 
             new_plan.extend(regen_result)
@@ -189,48 +261,11 @@ class Replanner:
         Generates a list (denoted plan) of four [{category: [group_words] }] using remaining words
             Returns None if not all of the groups make sense, otherwise returns the plan which is list [{category: [group_words] }] of remaining words
         '''
-        client = OpenAI()
-        system_prompt = f'''You are a smart NYT Connections puzzle solver. You are given a set of words and\
-            from those words you will generate groups of four words, where each group has some category theme and\
-                  the groups have no overlapping words.
-        
-        Return the groups as JSON object with a single key 'groups' whose value is an array of objects. Each object should have a single key-value pair, where the key is the category theme and the value is an array of 4 words in the group.
-        word_set = ['CAMPAIGN', 'CANVASS', 'CLAMP', 'COMPOSITION', 'FABRIC', 'FILE', 'LEVEL', 'LOG', 'MAKEUP', 'MAX', 'MOD', 'ORGANIZE', 'SAW', 'STRUCTURE', 'STUMP', 'TAN']
-        Return:
-        {{"groups": [
-            {{
-                "WAYS TO SUPPORT A CANDIDATE": ["CAMPAIGN", "CANVASS", "ORGANIZE", "STUMP"]
-            }},
-            {{
-                "CONSTITUTION": ["COMPOSITION", "FABRIC", "MAKEUP", "STRUCTURE"]
-            }},
-            {{
-                "CARPENTRY TOOLS": ["CLAMP", "FILE", "LEVEL", "SAW"]
-            }},
-            {{
-                "MATH ABBREVIATIONS": ["LOG", "MAX", "MOD", "TAN"]
-            }}
-        ]}}
-        Always return valid JSON in this format.
-        '''
-        
-        user_prompt = f"Set of words to generate groups of four from: {remaining_words}"
-        history = [
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt}
-            ]
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=history,
-            response_format={ "type": "json_object" },
-        )
-        response_msg = response.choices[0].message.content
-        response_json = json.loads(response_msg)
-        if isinstance(response_json, dict) and 'groups' in list(response_json.keys()):
-            plan = response_json['groups']
-        else:
-            raise ValueError(f"GPT generated the wrong JSON output. {response_msg}")
-        
+        user_prompt = f'Set of words to generate groups of four from: """{remaining_words}"""'
+        gpt_gen = GPT(user_prompt, replan_generator_system_prompt, self.failed_plans)
+        plan = gpt_gen.forward(remaining_words)
+        print(f"Words: {remaining_words}\n Regenerated Plan: {plan}\n")
+      
         # evaluate the generated groups
         is_valid_arr = self.jury.get_verdict(plan)
         num_valid = sum(map(int, is_valid_arr))
@@ -241,6 +276,8 @@ class Replanner:
         return plan 
        
     
+
+    
     def driver(self):
         generated_result = None
         while(generated_result is None):
@@ -249,7 +286,7 @@ class Replanner:
         
         # try out the given plan 
         # TODO: rank the elements in the given plan or use the votes by the jury to decide which one to try out first
-        
+        return generated_result
 
 
         
